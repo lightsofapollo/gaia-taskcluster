fbdea1f024d7426b5a8375703fb7ecebbb793000
James Lal <james@lightsofapollo.com>
2014-03-21 02:31:45 -0700

    update ami again

--- graph/github_pr.js
+++ graph/github_pr.js
@@ -10,7 +10,7 @@ var DEFAULT_PROVISIONER =
   process.env.TASKCLUSTER_PROVISIONER_ID || 'aws-provisioner';
 
 var DEFAULT_WORKER_TYPE =
-  process.env.TASKCLUSTER_WORKER_TYPE || 'ami-4a4e277a';
+  process.env.TASKCLUSTER_WORKER_TYPE || 'ami-f44128c4';
 
 /**
 Fetch the graph (but do not decorate it) from the pull request.
